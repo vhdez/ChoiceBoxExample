effd5da174ebdeb2eb603ecf7c4e88d2f2d7f3b7
vhdez <vhernandez@scienceleadership.org>
2017-10-15 23:54:23 -0400

    Change ChoiceBox to ComboBox

--- src/edu/sla/Main.java
+++ src/edu/sla/Main.java
@@ -5,7 +5,7 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -16,19 +16,19 @@ import javafx.stage.Stage;
 public class Main extends Application {
     // static variables ONLY needed by RickRoll_click method
     //                   NOT needed by ChoiceBox's changed method
-    static Image     imageAhead;
-    static ImageView imageView;
-    static ChoiceBox cb;
+    private static Image     imageAhead;
+    private static ImageView imageView;
+    private static ComboBox comboBox;
 
-    public static void RickRoll_click() {
+    private static void RickRoll_click() {
         // button clicked, so show Rick looking ahead
         imageView.setImage(imageAhead);
         // turn on the choicebox
-        if (cb.isDisabled()) {
-            cb.setDisable(false);
+        if (comboBox.isDisabled()) {
+            comboBox.setDisable(false);
         }
         // Make sure my choicebox's selection matches with image
-        cb.setValue("Ahead");
+        comboBox.setValue("Ahead");
     }
 
     //@Override
@@ -39,6 +39,7 @@ public class Main extends Application {
         Image imageRight = new Image("RickRight.png", 800 , 500, false ,true);
         Image imageDown = new Image("RickRollDown.png", 800, 500, false, true);
         Image imageUp = new Image("RickRollUp.jpg", 800, 500, false, true);
+        Image imageSecret = new Image("RickRollSecret.jpg", 800, 500, false, true);
         // Create imageView where 5 images will be displayed (initially NO image is displayed)
         imageView = new ImageView();
 
@@ -47,46 +48,52 @@ public class Main extends Application {
         // When button is clicked, display RickRoll image, enable choicebox, and make choicebox match image
         RickRoll.setOnAction(e -> RickRoll_click());
 
-        // Create choicebox that lets user choose between 5 types of Rick-Rolling
-        cb = new ChoiceBox(FXCollections.observableArrayList("Ahead", "Left", "Right", "Up", "Down"));
-        // Add "changed" method that listens for choicebox selection changing
-        cb.getSelectionModel().selectedIndexProperty().addListener(
-                new ChangeListener<Number>() {
-                    public void changed(ObservableValue ov, Number value, Number new_value) {
-                        // when choicebox selection changes, match image to selection number
-                        if (new_value.intValue() == 0) {
-                            // choicebox had "Ahead" selected
-                            imageView.setImage(imageAhead);
-                        } else if (new_value.intValue() == 1) {
-                            // choicebox had "Left" selected
-                            imageView.setImage(imageLeft);
-                        } else if (new_value.intValue() == 2) {
-                            // choicebox had "Right" selected
-                            imageView.setImage(imageRight);
-                        } else if (new_value.intValue() == 3) {
-                            // choicebox had "Up" selected
-                            imageView.setImage(imageUp);
-                        } else if (new_value.intValue() == 4) {
-                            // choicebox had "Down" selected
-                            imageView.setImage(imageDown);
-                        }
-                    }
-                });
-        // initially, choicebox is disabled until RickRolling button is pressed
-        cb.setDisable(true);
+        // Create comboBox that lets user choose between 5 types of Rick-Rolling
+        comboBox = new ComboBox(FXCollections.observableArrayList("Ahead", "Left", "Right", "Up", "Down"));
+        comboBox.setPromptText("Direction");
+        comboBox.setEditable(true);
+        // Add "changed" method that listens for comboBox selection changing
+        comboBox.valueProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue ov, String oldString, String newString) {
+                // when comboBox selection changes, match image to selection number
+                if (newString.equals("Ahead")) {
+                    // comboBox had "Ahead" selected
+                    imageView.setImage(imageAhead);
+                } else if (newString.equals("Left")) {
+                    // comboBox had "Left" selected
+                    imageView.setImage(imageLeft);
+                } else if (newString.equals("Right")) {
+                    // comboBox had "Right" selected
+                    imageView.setImage(imageRight);
+                } else if (newString.equals("Up")) {
+                    // comboBox had "Up" selected
+                    imageView.setImage(imageUp);
+                } else if (newString.equals("Down")) {
+                    // comboBox had "Down" selected
+                    imageView.setImage(imageDown);
+                } else if (newString.equalsIgnoreCase("Secret")) {
+                    // comboBox had "Secret" typed
+                    imageView.setImage(imageSecret);
+                }
+            }
+        });
+
+        // initially, comboBox is disabled until RickRolling button is pressed
+        comboBox.setDisable(true);
 
         // Use a BorderPane as the root for scene
         // A border pane has 5 regions to put controls: Top, Bottom, Left, Right, and Center
         BorderPane border = new BorderPane();
 
-        // Create AnchorPane for RickRoll button and choicebox
-        //    Use AnchorPane instead of HBox to display choicebox on right edge of window
+        // Create AnchorPane for RickRoll button and comboBox
+        //    Use AnchorPane instead of HBox to display comboBox on right edge of window
         AnchorPane topAnchorPane = new AnchorPane();
-        topAnchorPane.getChildren().addAll(RickRoll, cb);
+        topAnchorPane.getChildren().addAll(RickRoll, comboBox);
         // Anchor RickRoll button on left after 5.0 pixels from leftmost edge
         AnchorPane.setLeftAnchor(RickRoll, 5.0);
-        // Anchor Choicebox  on right before 5.0 pixels to rightmost edge
-        AnchorPane.setRightAnchor(cb, 5.0);
+        // Anchor comboBox on right before 5.0 pixels to rightmost edge
+        AnchorPane.setRightAnchor(comboBox, 5.0);
         // Set the Top region of the BorderPane to be this AnchorPane
         border.setTop(topAnchorPane);
 
